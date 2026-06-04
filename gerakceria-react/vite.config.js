77021858b983a370b8dd597197dc747e662e56b0
Ayi Sukmana <20241510210@uniku.ac.id>
2026-06-04 13:21:28 +0700

    Fix GitHub Pages

--- gerakceria-react/vite.config.js
+++ gerakceria-react/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/gerak-ceria-redesigned-v2/',
   plugins: [react()],
 })
